ad5baff3fc8dc8c86768d03e9ce0e882fb82f08e
Peter Traversa <peter.traversa@gmail.com>
2018-06-13 17:10:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string === string.toLowerCase
-  return "I can't hear you"
+  if (string === string.toLowerCase()) {
+   return "I can't hear you." 
+  }
+  if (string === string.toUpperCase()) {
+    return ""
+  }
 }
